790b98c89a07cd9191a7b7cc5279356801f69140
Ada <bonivada@gmail.com>
2025-01-06 16:20:21 +0100

    V3

--- src/main/java/fr/eseo/calculatrice/CalculatriceController.java
+++ src/main/java/fr/eseo/calculatrice/CalculatriceController.java
@@ -1,14 +1,27 @@
 package fr.eseo.calculatrice;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class CalculatriceController {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class CalculatriceController implements PropertyChangeListener {
     @FXML
-    private Label welcomeText;
+    private TextField num1;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField num2;
+
+    @FXML
+    private Label Result;
+
+    public Button additionBouton;
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
     }
 }
