5c875feebc3e7ab11931b57d62e2faa02ec3a568
Mohamed BOHOU <mohamedbohou574@gmail.com>
2025-01-14 16:52:10 +0100

    V18

--- src/main/java/fr/eseo/calculatrice/CalculatriceApplication.java
+++ src/main/java/fr/eseo/calculatrice/CalculatriceApplication.java
@@ -11,9 +11,10 @@ public class CalculatriceApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatriceApplication.class.getResource("calculatrice-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 260, 240);
         stage.setTitle("Calculatrice");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
